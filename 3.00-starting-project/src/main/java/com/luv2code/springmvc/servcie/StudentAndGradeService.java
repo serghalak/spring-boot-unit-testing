8443536252a22fb1ce7cd1a71f434e89cfc55a51
serghalak <serghalak@gmail.com>
2022-10-09 14:59:06 +0300

    created integration test with service and dao and load data from file

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/servcie/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/servcie/StudentAndGradeService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class StudentAndGradeService {
@@ -17,5 +19,23 @@ public class StudentAndGradeService {
         CollegeStudent student = new CollegeStudent(firstName, lastName, emailAddress);
         student.setId(0);
         studentDao.save(student);
+    }
+
+    public boolean checkIfStudentIsNull(int id) {
+        final Optional<CollegeStudent> byId = studentDao.findById(id);
+        if (byId.isPresent()) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public void deleteStudent(int id) {
+        if (checkIfStudentIsNull(id))
+            studentDao.deleteById(id);
+    }
+
+    public Iterable<CollegeStudent> getGradebook() {
+        return studentDao.findAll();
     }
 }
